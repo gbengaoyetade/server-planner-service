44f8d89a98f5ce45e113c32d378196e043f83a4f
gbengaPS <ioyetade@gmail.com>
2020-08-25 17:05:48 +0100

    add route validation

---
+++ src/helpers/routeValidator.js
@@ -0,0 +1,41 @@
+const hasRequiredFields = (body) => {
+  if('server' in body && 'virtualMachines' in body){
+    return true;
+  }
+  return false;
+}
+
+const serverFieldIsValid = (server) => {
+  if(server.CPU ===undefined || isNaN(server.CPU)) return false
+  if(server.HDD ===undefined || isNaN(server.HDD)) return false
+  if(server.RAM ===undefined || isNaN(server.RAM)) return false
+  return true;
+}
+
+const virtualMachinesAreValid = (virtualMachines) => {
+  if(!Array.isArray(virtualMachines)) return false;
+
+  const hasErrors = virtualMachines.some((machine) => {
+   return !serverFieldIsValid(machine)
+  });
+
+  return !hasErrors;
+}
+
+exports.validateServerCapacityRoute = (req, res, next) => {
+  const { body } = req;
+  if(!hasRequiredFields(body)) {
+    return res.status(400).send({ error: "'server' and 'virtualMachines' fields are required" })
+  }
+
+  if(!serverFieldIsValid(body.server)) {
+    return res.status(400).send({ error: 'server field has invalid keys or values' })
+  }
+
+  if(!virtualMachinesAreValid(body.virtualMachines)){
+    return res.status(400).send({ error: 'virtualMachines array contains wrongly formatted inputs'})
+  }
+
+  return next();
+}
+
--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const { computeServerCapacity } = require('./handlers')
+const { computeServerCapacity } = require('./handlers');
+const { validateServerCapacityRoute } = require('./helpers/routeValidator');
 
 const app = express();
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send({message: 'Welcome to server planner service'});
-})
+  res.send({ message: 'Welcome to server planner service' });
+});
 
-app.post('/api/server', computeServerCapacity)
+app.post('/api/server/capacity', validateServerCapacityRoute, computeServerCapacity);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ error: 'Route not found' })
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
